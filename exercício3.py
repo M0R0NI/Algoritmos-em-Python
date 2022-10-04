fae2664c0f8652a24ba4b5c4cd51cd79990f193d
M0R0NI <114964551+M0R0NI@users.noreply.github.com>
2022-10-04 00:31:49 -0300

    removendo arquivo

--- exercício3.py
+++
@@ -1,8 +0,0 @@
-"""
-Construir um programa que apresente a soma dos cem primeiros números naturais.
-
-"""
-soma_n = 1
-for naturais in range(1, 101):
-    soma_n = soma_n + naturais
-    print(soma_n)
--- exercício6.py
+++
@@ -1,8 +0,0 @@
-"""
-Construir um programa que apresente todos os valores numéricos divisíveis por 4 e menores que 200.
-
-"""
-
-for i in range(1, 200):
-    if i%4 == 0:
-        print(i)
